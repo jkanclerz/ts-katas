ecf488aa1795d8cbe10af76819f15c4b44371f4f
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2019-04-06 14:56:03 +0200

    add gross to nett scenario

--- src/pricing/pricing.test.ts
+++ src/pricing/pricing.test.ts
@@ -3,11 +3,28 @@ import { PriceCalculator,ItemPrice, CalcRequest} from './pricing';
 describe('allow calculate pricing based on input', () => {
     it('calculate gross based on nett + tax', () => {
       const calc = new PriceCalculator();
-      const calcRequest = {
-        net: 100,
+      const calculationRequest = {
+        netto: 100,
+        gross: null,
         tax: 0.23 
       }
-      const res = calc.calculate(calcRequest)
+
+      const res = calc.calculate(calculationRequest)
+      expect(res instanceof ItemPrice).toBeTruthy();
+      expect(res.netto()).toBe(100.00);
+      expect(res.gross()).toBe(123.00);
+      expect(res.tax()).toBe(0.23);
+    });
+
+    it('calculate nett based on gross', () => {
+      const calc = new PriceCalculator();
+      const calculationRequest = {
+        netto: null,
+        gross: 123,
+        tax: 0.23 
+      }
+
+      const res = calc.calculate(calculationRequest)
       expect(res instanceof ItemPrice).toBeTruthy();
       expect(res.netto()).toBe(100.00);
       expect(res.gross()).toBe(123.00);
